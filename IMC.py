db7826c273c6d90b54a6a47e9364413dcb419b6c
WelentonNG <welenton24@gmail.com>
2025-04-10 13:54:10 -0300

    Add files via upload

---
+++ IMC.py
@@ -0,0 +1,5 @@
+kg=float(input(("Qual é seu peso ")))
+altura=float(input(("Qual é sua altura ")))
+imc=kg/ (altura ** 2)
+
+print(imc)
---
+++ Porva.py
@@ -0,0 +1,20 @@
+
+p1=float(input("Digite a nota da primeira prova (0-10) "))
+
+p2=float(input("Digite a nota da segunda  prova (0-10) "))
+
+media = (p1+p2) /2
+
+
+if media  >= 7:
+  print("aluno aprovado")
+
+elif media >=4 :
+  print("aluno em recuperação ")
+
+
+else :
+    print("aluno reprovado")
+
+
+
